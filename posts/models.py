c2dcb12e9bed8c1bcb28a9d02b14c644103d68a1
PressXToWin <PressXToWin@users.noreply.github.com>
2023-12-04 16:50:13 +0700

    refactoring, add posts to admin

--- posts/models.py
+++ posts/models.py
@@ -9,8 +9,6 @@ class Post(models.Model):
     pub_date = models.DateTimeField(verbose_name='Publication date', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
-    file = models.FileField(
-        upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
         return self.text
